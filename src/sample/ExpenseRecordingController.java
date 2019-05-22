413091c65cca99d7db10fe4d8094983b6d3e5925
Almanbet Erkinbek uulu <almanbet.erkinbekuulu@iaau.edu.kg>
2019-05-23 01:11:46 +0600

    3rd page - income recording page and 4th page - expense recording page are added

---
+++ src/sample/ExpenseRecordingController.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ExpenseRecordingController {
+
+    @FXML
+    private TextField expense;
+
+    @FXML
+    private TextField descriptionOfExpense;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    void cancelButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonPressed(ActionEvent event) {
+
+    }
+
+}
---
+++ src/sample/IncomeRecordingController.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class IncomeRecordingController {
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TextField newIncome;
+
+    @FXML
+    void cancelButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonPressed(ActionEvent event) {
+
+    }
+
+}
