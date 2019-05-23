229f0ef6af4bc9a152f5af2ac391cc66b5301f50
Almanbet Erkinbek uulu <almanbet.erkinbekuulu@iaau.edu.kg>
2019-05-23 13:56:13 +0600

    Making the first page work

--- src/sample/InfoPageController.java
+++ src/sample/InfoPageController.java
@@ -2,11 +2,16 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 
-public class InfoPageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InfoPageController implements Initializable{
 
     @FXML
     private Label firstNameLastName;
@@ -32,6 +37,13 @@ public class InfoPageController {
     @FXML
     private ListView<?> expenseList;
 
+    //It will take the value from previous class
+    public void valueTaker(String firstName, String lastName, int balance, String currencyType){
+        this.firstNameLastName.setText(firstName +" "+ lastName);
+        this.balance.setText("" + balance);
+        this.currencyType.setText(currencyType);
+    }
+
     @FXML
     public void expenseAddButtonPressed(ActionEvent event) {
 
@@ -40,6 +52,12 @@ public class InfoPageController {
     @FXML
     public void incomeAddButtonPressed(ActionEvent event) {
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        income.setText("0");
+        expense.setText("0");
     }
 
 }
--- src/sample/WelcomingPageController.java
+++ src/sample/WelcomingPageController.java
@@ -1,13 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
+import javax.security.auth.callback.ConfirmationCallback;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static java.awt.SystemColor.window;
 
 public class WelcomingPageController implements Initializable {
 
@@ -38,12 +49,71 @@ public class WelcomingPageController implements Initializable {
     @FXML
     private Button exitButton;
 
-    public void submitButtonPressed(){}
+    private ToggleGroup currencyRBgroup;
 
-    public void cancelButtonPressed(){}
+    public String getFirstName(){
+        return firstName.getText();
+    }
+
+    public String getLastName(){
+        return lastName.getText();
+    }
+
+    public int getBalance(){
+        return Integer.parseInt(balance.getText());
+    }
+
+    public String getCurrency(){
+        String currency = "";
+
+        if (this.currencyRBgroup.getSelectedToggle().equals(this.somRB)){
+            currency = "Som";
+        }
+        if (this.currencyRBgroup.getSelectedToggle().equals(this.dollarRB)){
+            currency = "Dollar";
+        }
+        if (this.currencyRBgroup.getSelectedToggle().equals(this.euroRB)){
+            currency = "Euro";
+        }
+        if (this.currencyRBgroup.getSelectedToggle().equals(this.yuanRB)){
+            currency = "Yuan";
+        }
+
+        return currency;
+    }
+
+    public void submitButtonPressed(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("infoPage.fxml"));
+        Parent newPage = loader.load();
+
+        Scene tableViewScene = new Scene(newPage);
+
+        //access the controller, it will pass this class's instance variable to the next class
+        InfoPageController controller = loader.getController();
+        controller.valueTaker(getFirstName(), getLastName(), getBalance(), getCurrency());
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+        window.show();
+    }
+
+    public void cancelButtonPressed(ActionEvent event) throws  IOException {
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.close();
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb){
+        //configuring and setting up the currency radio buttons in one toggle
+        currencyRBgroup = new ToggleGroup();
+        this.somRB.setToggleGroup(currencyRBgroup);
+        this.dollarRB.setToggleGroup(currencyRBgroup);
+        this.euroRB.setToggleGroup(currencyRBgroup);
+        this.yuanRB.setToggleGroup(currencyRBgroup);
 
     }
 
